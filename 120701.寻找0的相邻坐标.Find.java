2692f2af2a996efa65f337792411d5019c8d6650
Xhhhhhzsr <48664721+Xhhhhhzsr@users.noreply.github.com>
2020-12-07 15:50:49 +0800

    Update 120701.寻找0的相邻坐标.Find.java

--- 120701.寻找0的相邻坐标.Find.java
+++ 120701.寻找0的相邻坐标.Find.java
@@ -1,14 +1,14 @@
 package Array;
 //120701.寻找0的相邻坐标.Find.java
 public class ArrayTest {
-	 int R_x=0;//行 row
-	 int R_y=0;//列 column
+	 int R_x=0;//列 column 
+	 int R_y=0;//行 row
 //	R_Zero=(j,i);
 	void FinnZero(int array2d[][]){
 		for (int row = 0;row < array2d.length; row++) {
 			for (int culumn = 0; culumn < array2d[0].length; culumn++) {
 				if (array2d[row][culumn]==0) {
-					System.out.println("0的坐标是:"+"R_Zero("+row+","+culumn+")");
+					System.out.println("0的坐标是:"+"R_Zero("+culumn+","+row+")");
 				}
 			
 			}
@@ -19,9 +19,9 @@ public class ArrayTest {
 		for (int row = 0;row < array2d.length; row++) {
 			for (int culumn = 0; culumn < array2d[0].length; culumn++) {
 				if (array2d[row][culumn]==0) {
-					R_x=row;
-					R_y=culumn;
-					System.out.println("0的左边数字"+10+"的坐标是:"+"R_left("+(R_x)+","+(R_y-1)+")");
+					int R_x=culumn;
+					int R_y=row;
+					System.out.println("0的左边数字"+10+"的坐标是:"+"R_left("+(R_x-1)+","+(R_y)+")");
 				}
 			
 			}
@@ -32,9 +32,9 @@ public class ArrayTest {
 		for (int row = 0;row < array2d.length; row++) {
 			for (int culumn = 0; culumn < array2d[0].length; culumn++) {
 				if (array2d[row][culumn]==0) {
-					R_x=row;
-					R_y=culumn;
-					System.out.println("0的右边数字"+12+"的坐标是:"+"R_right("+(R_x)+","+(R_y+1)+")");
+					int R_x=culumn;
+					int R_y=row;
+					System.out.println("0的右边数字"+12+"的坐标是:"+"R_right("+(R_x+1)+","+(R_y)+")");
 				}
 			
 			}
@@ -47,9 +47,9 @@ public class ArrayTest {
 		for (int row = 0;row < array2d.length; row++) {
 			for (int culumn = 0; culumn < array2d[0].length; culumn++) {
 				if (array2d[row][culumn]==0) {
-					R_x=row;
-					R_y=culumn;
-					System.out.println("0的上边数字"+5+"的坐标是:"+"R_right("+(R_x-1)+","+(R_y)+")");
+					int R_x=culumn;
+					int R_y=row;
+					System.out.println("0的上边数字"+5+"的坐标是:"+"R_up("+(R_x)+","+(R_y-1)+")");
 				}
 			
 			}
@@ -62,9 +62,9 @@ public class ArrayTest {
 		for (int row = 0;row < array2d.length; row++) {
 			for (int culumn = 0; culumn < array2d[0].length; culumn++) {
 				if (array2d[row][culumn]==0) {
-					R_x=row;
-					R_y=culumn;
-					System.out.println("0的下边数字"+11+"的坐标是:"+"R_right("+(R_x+1)+","+(R_y)+")");
+					int R_x=culumn;
+					int R_y=row;
+					System.out.println("0的下边数字"+11+"的坐标是:"+"R_down("+(R_x)+","+(R_y+1)+")");
 				}
 			
 			}
